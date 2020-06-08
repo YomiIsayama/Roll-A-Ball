e966e9d799dda28650ae3901cf2e497afdc2027d
YomiIsayama <scepters1@gmail.com>
2020-06-08 11:51:58 +0800

    test

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Launcher : MonoBehaviour
 {
     public GameObject ballPrefab;
+    
+
     //public Ball ballcs;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
